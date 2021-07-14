65c89bea228a54970384da3f4dee91efa2f82414
Nishtha-236 <mahajannishtha236@gmail.com>
2021-07-14 17:31:56 +0530

    '1st'

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- <h1>{{ msg }}</h1> -->
+  <div></div>
+  <!-- <p>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank">
+      Vite Documentation
+    </a>
+    |
+    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+  </p>
+
+  <button type="button" @click="state.count++">
+    count is: {{ state.count }}
+  </button>
+  <p>
+    Edit
+    <code>components/HelloWorld.vue</code> to test hot module replacement.
+  </p> -->
+</template>
+
+<script>
+// import { computed, defineProps, reactive } from 'vue'
+import axios from 'axios'
+
+export default {
+
+  data(){
+    return{
+      code:""
+    }
+  },
+  async mounted() {
+    console.log(this.$route.query.code)
+    this.code = this.$route.query.code
+    // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+    await axios.post('https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token', {}, {params: {
+      grant_type:'authorization_code',
+      redirect_uri: 'http://localhost:3000',
+      code: this.code,
+      client_id: '2c2944bf-0add-4ed0-9e4d-eba58cc91040',
+      state:'1234'
+    }})
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    }
+}
+// const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+/* a {
+  color: #42b983;
+} */
+</style>
