3dede6f55f45de91d71fab6ac75eac2a177e06d3
Karthik Nishanth <karthik.nishanth06@gmail.com>
2021-07-14 22:49:50 +0530

    added access token flow

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
   <router-view />
 </template>
 
 <script setup>
 // // import HelloWorld from './components/HelloWorld.vue'
-
 
 // This starter template is using Vue 3 experimental <script setup> SFCs
 // Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,13 @@
 <template>
   <!-- <h1>{{ msg }}</h1> -->
-  <div></div>
+  <div>
+    <button v-if="code == undefined">
+      <a
+        href="https://fhir.epic.com/interconnect-fhir-oauth/oauth2/authorize?response_type=code&redirect_uri=http://localhost:3000&client_id=2c2944bf-0add-4ed0-9e4d-eba58cc91040&state=1234"
+        >Sign in</a
+      >
+    </button>
+  </div>
   <!-- <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
       Vite Documentation
@@ -20,34 +27,55 @@
 
 <script>
 // import { computed, defineProps, reactive } from 'vue'
-import axios from 'axios'
+import axios from "axios";
 
 export default {
-
-  data(){
-    return{
-      code:""
-    }
+  data() {
+    return {
+      code: "",
+      accesstoken: "",
+    };
   },
   async mounted() {
-    console.log(this.$route.query.code)
-    this.code = this.$route.query.code
-    // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-    await axios.post('https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token', {}, {params: {
-      grant_type:'authorization_code',
-      redirect_uri: 'http://localhost:3000',
-      code: this.code,
-      client_id: '2c2944bf-0add-4ed0-9e4d-eba58cc91040',
-      state:'1234'
-    }})
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    console.log(this.$route.query.code);
+    this.code = this.$route.query.code;
+    if (this.code != undefined) {
+      const params = new URLSearchParams();
+      params.append("grant_type", "authorization_code");
+      params.append("redirect_uri", "http://localhost:3000");
+      params.append("code", this.code);
+      params.append("client_id", "2c2944bf-0add-4ed0-9e4d-eba58cc91040");
+      params.append("state", "1234");
+      const config = {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      };
+      await axios
+        .post(
+          "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token",
+          params,
+          config
+        )
+        .then((response) => (this.accesstoken = response.data.access_token))
+        .catch(function (error) {
+          console.log(error);
+        });
     }
-}
+    if (this.accesstoken != "") {
+      axios
+        .get(
+          `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Patient/${this.accesstoken}`
+        )
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
+};
 // const state = reactive({ count: 0 })
 </script>
 
