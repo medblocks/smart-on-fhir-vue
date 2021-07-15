8c5f632099568b39fffc2740fc2d0ff617a99f5a
Sidharth Ramesh <tornadoalert@gmail.com>
2021-07-15 10:41:59 +0530

    inject env variables

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+const viteEnv = {}
+Object.keys(process.env).forEach((key) => {
+  viteEnv[`import.meta.env.${key}`] = process.env[key]
+})
+
+
 export default defineConfig({
+  define: viteEnv,
   plugins: [vue()]
 })
