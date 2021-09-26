ed712de7c3c63b3fab57919f3e540421397c6920
Sidharth Ramesh <tornadoalert@gmail.com>
2021-09-26 16:51:35 +0530

    removed package.lock

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -73,7 +73,7 @@ export default {
       accesstoken: "",
       patient: "",
       patientdata: {},
-      clientId: "42f3b173-16a8-4c50-a3ea-0269294cb869",
+      clientId: "42f3b173-16a8-4c50-a3ea-0269294cb869", // Replace with your client id
       redirect: import.meta.env.PROD
         ? "https://lucid-wozniak-940eae.netlify.app"
         : "http://localhost:3000",
